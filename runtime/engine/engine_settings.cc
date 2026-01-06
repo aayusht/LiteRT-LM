2f0804ffe7c2aef2939f013b936bee5d2a39fb97
Marissa Ikonomidis <marissaw@google.com>
2026-01-06 15:09:01 -0800

    Do not dump engine settings by default
    The engine settings are primarily useful for the developers of LiteRT LM
    and power user of LiteRT LM. However, they are not used by the average
    user and they are very, very verbose which makes it difficult to find
    the output of the model or to look back at previous calls to the model.
    
    Users can use --minloglevel=5 to view the logs
    
    LiteRT-LM-PiperOrigin-RevId: 852952681

--- runtime/engine/engine_settings.cc
+++ runtime/engine/engine_settings.cc
@@ -23,6 +23,7 @@
 #include <vector>
 
 #include "absl/base/nullability.h"  // from @com_google_absl
+#include "absl/log/absl_log.h"  // from @com_google_absl
 #include "absl/status/status.h"  // from @com_google_absl
 #include "absl/status/statusor.h"  // from @com_google_absl
 #include "absl/strings/match.h"  // from @com_google_absl
@@ -256,7 +257,7 @@ absl::Status EngineSettings::MaybeUpdateAndValidate(
         audio_executor_settings_.value(), audio_backend_constraint, "Audio"));
   }
 
-  ABSL_LOG(INFO) << "The llm metadata: " << metadata.DebugString();
+  ABSL_VLOG(5) << "The llm metadata: " << metadata.DebugString();
   ABSL_LOG(INFO) << "The validated engine settings: " << *this;
   return absl::OkStatus();
 }
@@ -371,11 +372,10 @@ SessionConfig SessionConfig::CreateDefault() {
 
 absl::Status SessionConfig::MaybeUpdateAndValidate(
     const EngineSettings& engine_settings) {
-  ABSL_LOG(INFO)
-      << "The GetLlmMetadata: "
-      << (engine_settings.GetLlmMetadata().has_value()
-              ? engine_settings.GetLlmMetadata().value().DebugString()
-              : "Not set");
+  ABSL_VLOG(5) << "The GetLlmMetadata: "
+               << (engine_settings.GetLlmMetadata().has_value()
+                       ? engine_settings.GetLlmMetadata().value().DebugString()
+                       : "Not set");
   if ((stop_token_ids_.empty()) &&
       !engine_settings.GetLlmMetadata().has_value()) {
     return absl::InvalidArgumentError(
@@ -461,7 +461,7 @@ absl::Status SessionConfig::MaybeUpdateAndValidate(
     }
   }
 
-  ABSL_LOG(INFO) << "The validated session config: " << *this;
+  ABSL_VLOG(5) << "The validated session config: " << *this;
   return absl::OkStatus();
 }
 
